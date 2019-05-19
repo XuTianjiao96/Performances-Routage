fe0119fd2b51624258e5da15df751936303183d8
Jiaming <424366965@qq.com>
2019-05-19 08:23:47 +0800

    Algo MinHop fini.  Yo!

--- calculation.cpp
+++ calculation.cpp
@@ -45,10 +45,36 @@ int Calculation::getHopCount(int fromId, int toId)
 	string switchName = fromHost->dstName;
 	
 	//TODO
+	string nextNode = switchName;
+	cout << "nextNode: " << nextNode << endl;
+	int outPort = 0;
 	//Parcourir la table de routage du noeud fromId vers le toId
 	//Aide : Regarder la struture routeItem et switchNode elles pourront vous aider.
 	//Aide : struct routeItem * item = routingTable.getTableByName(switchName); permet de charger la table de routage
-	//Retourner le nombre de sauts du noeud fromId vers le toId	
+	while ( nextNode.compare(toNode) != 0) {  
+
+		struct routeItem * item = routingTable.getTableByName(nextNode);
+		
+		for (int i = 0; i < item->dstInfo.size(); i++) {
+			//cout << "dstinfo[" << i << "] = " << item->dstInfo[i] << endl;
+			if (item->dstInfo[i].compare(toNode) == 0) {
+				outPort = item->outport[i];
+			}
+		}
+		cout << "outPort: " << outPort << endl;		
+
+		struct switchNode * node = topologyTable.getSwitchByName(nextNode);
+
+		for (int i = 0; i < node->srcPort.size(); i++) {
+			if (node->srcPort[i] == outPort) {
+				nextNode = node->dstName[i];
+			}
+		}
+		cout << "nextNode: " << nextNode << endl;
+		 
+		//Retourner le nombre de sauts du noeud fromId vers le toId	
+		count++;
+	}
 	cout << "From " << fromNode << " to " << toNode << ": Hop Count = " << count << endl;
 	return count;
 }
@@ -60,10 +86,23 @@ int Calculation::calculate()
 {
 	int minHop = 0; //Min value
 
+	//minHop = getHopCount(1, 9);
 	//TODO 
 	//Implémenter l'algo du calcul du nombre de sauts
 	//Aide : Pour recueperer le nombre de saut de source vers destination -> getHopCount(source, destination);
 	//Aide : Pour recuperer le nombre de HCA -> topologyTable.getHostCount();
+	int cpt = 0;
+	int hopCount = 0;
+	for ( int src = 0; src < topologyTable.getHostCount(); src++) {
+		cpt = 0;
+		minHop = 0;
+		for ( int dest = 0; dest < topologyTable.getHostCount(); dest++) {
+			if (src == dest) continue;
+			hopCount = getHopCount(src, dest);
+			cpt = max(cpt, hopCount);
+		}
+		minHop = max(minHop, cpt);
+	}
 	//retourner le minimum trouver.
 
 	return minHop;
